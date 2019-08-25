164ca487251677a2948ffc23ef89f842bf02d05c
Vladyslav Kudelia <vladyslavkudelia@Vladyslavs-Mac-Pro.local>
2019-08-25 19:50:59 +0300

    Add new elements

---
+++ HEREMapsExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HEREMapsExample
+//
+//  Created by Vladyslav Kudelia on 8/25/19.
+//  Copyright © 2019 Vladyslav Kudelia. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
